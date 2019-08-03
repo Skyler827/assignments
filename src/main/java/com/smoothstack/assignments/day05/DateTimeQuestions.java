63d89ab1190abae917bd17fbd142e80838157ba0
Skyler Daché <skyberskyman@gmail.com>
2019-08-02 22:12:49 -0400

    Fixed a bug I think

--- src/main/java/com/smoothstack/assignments/day05/DateTimeQuestions.java
+++ src/main/java/com/smoothstack/assignments/day05/DateTimeQuestions.java
@@ -3,13 +3,19 @@ package com.smoothstack.assignments.day05;
 import java.util.Date;
 
 public class DateTimeQuestions {
-	public String whatClassWouldYouUseToStoreYourBirthday() {
+	static long millisecondsPerDay = 1000*60*60*24;
+	static public String whatClassWouldYouUseToStoreYourBirthday() {
 		return "LocalDateTime";
 	}
-	Date dateOfPreviousThursday(Date d) {
+	static Date dateOfPreviousThursday(Date d) {
 		int day = d.getDay();
 		int dayOffset = ((day+2)%7)+1;
-		int millisecondsPerDay = 1000*60*60*24;
 		return new Date(d.getTime()-millisecondsPerDay*dayOffset);
+	}
+	public static void main(String[] args) {
+		Date d = new Date();
+		long time_ = d.getTime()+500*millisecondsPerDay;
+		Date d2 = new Date(time_);
+		System.out.println(dateOfPreviousThursday(d2));
 	}
 }
